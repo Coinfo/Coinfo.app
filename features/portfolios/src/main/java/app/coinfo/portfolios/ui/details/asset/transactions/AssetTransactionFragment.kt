ca5be58fc0c62a7a6a14cb8f31b1dc5906f7b121
Viktor Apoyan <chamich.apps@gmail.com>
2022-01-14 22:45:26 +0100

    Added AssetInfo and AssetTransactions Fragment

---
+++ features/portfolios/src/main/java/app/coinfo/portfolios/ui/details/asset/transactions/AssetTransactionFragment.kt
@@ -0,0 +1,57 @@
+package app.coinfo.portfolios.ui.details.asset.transactions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import app.coinfo.portfolios.databinding.FragmentAssetTransactionBinding
+
+class AssetTransactionFragment : Fragment() {
+
+    private var _binding: FragmentAssetTransactionBinding? = null
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     * This is optional, and non-graphical fragments can return null. This will be called between
+     * {@link #onCreate(Bundle)} and {@link #onViewCreated(View, Bundle)}.
+     * <p>A default View can be returned by calling {@link #Fragment(int)} in your
+     * constructor. Otherwise, this method returns null.
+     *
+     * <p>It is recommended to <strong>only</strong> inflate the layout in this method and move
+     * logic that operates on the returned View to {@link #onViewCreated(View, Bundle)}.
+     *
+     * <p>If you return a View from here, you will later be called in
+     * {@link #onDestroyView} when the view is being released.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return Return the View for the fragment's UI, or null.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentAssetTransactionBinding.inflate(inflater, container, false).apply {
+        _binding = this
+    }.root
+
+    /**
+     * Called when the view previously created by {@link #onCreateView} has
+     * been detached from the fragment.  The next time the fragment needs
+     * to be displayed, a new view will be created.  This is called
+     * after {@link #onStop()} and before {@link #onDestroy()}.  It is called
+     * <em>regardless</em> of whether {@link #onCreateView} returned a
+     * non-null view.  Internally it is called after the view's state has
+     * been saved but before it has been removed from its parent.
+     */
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
