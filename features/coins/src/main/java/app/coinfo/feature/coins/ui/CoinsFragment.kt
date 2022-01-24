1abdcd544cd1263729fbc8da2641a5ead1c59c8c
Viktor Apoyan <chamich.apps@gmail.com>
2022-01-24 22:27:16 +0100

    Data Binding activated.

--- features/coins/src/main/java/app/coinfo/feature/coins/ui/CoinsFragment.kt
+++ features/coins/src/main/java/app/coinfo/feature/coins/ui/CoinsFragment.kt
@@ -1,43 +1,23 @@
 package app.coinfo.feature.coins.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import app.coinfo.feature.coins.databinding.FragmentCoinsBinding
+import by.kirich1409.viewbindingdelegate.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 internal class CoinsFragment : Fragment() {
 
-    private var _binding: FragmentCoinsBinding? = null
+    private val binding: FragmentCoinsBinding by viewBinding(FragmentCoinsBinding::bind)
+    private val model: CoinsViewModel by viewModels()
 
-    /**
-     * Called to have the fragment instantiate its user interface view.
-     * This is optional, and non-graphical fragments can return null. This will be called between
-     * {@link #onCreate(Bundle)} and {@link #onViewCreated(View, Bundle)}.
-     * <p>A default View can be returned by calling {@link #Fragment(int)} in your
-     * constructor. Otherwise, this method returns null.
-     *
-     * <p>It is recommended to <strong>only</strong> inflate the layout in this method and move
-     * logic that operates on the returned View to {@link #onViewCreated(View, Bundle)}.
-     *
-     * <p>If you return a View from here, you will later be called in
-     * {@link #onDestroyView} when the view is being released.
-     *
-     * @param inflater The LayoutInflater object that can be used to inflate
-     * any views in the fragment,
-     * @param container If non-null, this is the parent view that the fragment's
-     * UI should be attached to.  The fragment should not add the view itself,
-     * but this can be used to generate the LayoutParams of the view.
-     * @param savedInstanceState If non-null, this fragment is being re-constructed
-     * from a previous saved state as given here.
-     *
-     * @return Return the View for the fragment's UI, or null.
-     */
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = FragmentCoinsBinding.inflate(inflater, container, false).apply {
-        _binding = this
-    }.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = model
+    }
 }
---
+++ features/coins/src/main/java/app/coinfo/feature/coins/ui/CoinsViewModel.kt
@@ -0,0 +1,8 @@
+package app.coinfo.feature.coins.ui
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class CoinsViewModel @Inject constructor(
+
+): ViewModel()
