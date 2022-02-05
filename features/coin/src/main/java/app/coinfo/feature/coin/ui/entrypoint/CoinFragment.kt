b7d1cd1ccdfec9f063ea29ace2b781793bf495b1
Viktor Apoyan <chamich.apps@gmail.com>
2022-02-05 16:57:42 +0100

    Added Coin module and navigation to it when coin selected from the coins list.

---
+++ features/coin/src/main/java/app/coinfo/feature/coin/ui/entrypoint/CoinFragment.kt
@@ -0,0 +1,27 @@
+package app.coinfo.feature.coin.ui.entrypoint
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import app.coinfo.feature.coin.details.R
+
+internal class CoinFragment : Fragment(R.layout.fragment_coin) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_coin, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.textView).text = arguments?.getString("id")
+    }
+}
