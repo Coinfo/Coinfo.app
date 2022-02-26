1384791dae272fe41eac19574335dff45011cf61
Viktor Apoyan <chamich.apps@gmail.com>
2022-02-26 17:06:07 +0100

    Fixed spotless issue.

--- app/src/main/java/app/coinfo/CoinfoActivity.kt
+++ app/src/main/java/app/coinfo/CoinfoActivity.kt
@@ -3,9 +3,9 @@ package app.coinfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import app.coinfo.databinding.ActivityMainBinding
