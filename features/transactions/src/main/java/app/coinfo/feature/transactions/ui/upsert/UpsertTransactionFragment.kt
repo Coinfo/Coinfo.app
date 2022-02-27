1868a6d6453d21cdea8aa0c45803b2a2d6ed5d30
Viktor Apoyan <chamich.apps@gmail.com>
2022-02-27 21:41:01 +0100

    Added Upsert ViewModel

--- features/transactions/src/main/java/app/coinfo/feature/transactions/ui/upsert/UpsertTransactionFragment.kt
+++ features/transactions/src/main/java/app/coinfo/feature/transactions/ui/upsert/UpsertTransactionFragment.kt
@@ -3,12 +3,14 @@ package app.coinfo.feature.transactions.ui.upsert
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import app.coinfo.feature.transactions.R
 import app.coinfo.feature.transactions.databinding.TransactionsFragmentUpsertTransactionBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
 
 internal class UpsertTransactionFragment : Fragment(R.layout.transactions_fragment_upsert_transaction) {
 
+    private val viewModel: UpsertTransactionViewModel by viewModels()
     private val binding: TransactionsFragmentUpsertTransactionBinding by viewBinding(
         TransactionsFragmentUpsertTransactionBinding::bind
     )
@@ -17,5 +19,6 @@ internal class UpsertTransactionFragment : Fragment(R.layout.transactions_fragme
         super.onViewCreated(view, savedInstanceState)
 
         binding.lifecycleOwner = this
+        binding.viewModel = viewModel
     }
 }
