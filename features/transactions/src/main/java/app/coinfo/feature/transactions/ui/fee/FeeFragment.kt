c7f6719b171e31e568c3ffaef959cc107df90858
Viktor Apoyan <chamich.apps@gmail.com>
2022-03-05 22:01:13 +0100

    Added Fee Fragment

--- features/transactions/src/main/java/app/coinfo/feature/transactions/ui/fee/FeeFragment.kt
+++ features/transactions/src/main/java/app/coinfo/feature/transactions/ui/fee/FeeFragment.kt
@@ -1,4 +1,4 @@
-package app.coinfo.feature.transactions.ui.price
+package app.coinfo.feature.transactions.ui.fee
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,38 +6,37 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import app.coinfo.feature.transactions.R
-import app.coinfo.feature.transactions.databinding.TransactionsFragmentPriceBinding
+import app.coinfo.feature.transactions.databinding.TransactionsFragmentFeeBinding
 import app.coinfo.library.core.ktx.setBackStackData
-import app.coinfo.library.core.ktx.toString
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-internal class PriceFragment : BottomSheetDialogFragment() {
+class FeeFragment : BottomSheetDialogFragment() {
 
-    private val binding: TransactionsFragmentPriceBinding by viewBinding(TransactionsFragmentPriceBinding::bind)
-    private val args: PriceFragmentArgs by navArgs()
+    private val binding: TransactionsFragmentFeeBinding by viewBinding(TransactionsFragmentFeeBinding::bind)
+    private val args: FeeFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.transactions_fragment_price, container, false)
+    ): View? = inflater.inflate(R.layout.transactions_fragment_fee, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.lifecycleOwner = this
-        binding.editTextPricePerCoin.setText(args.price)
+        binding.editTextFee.setText(args.fee)
         setupClickListeners()
     }
 
     private fun setupClickListeners() {
-        binding.buttonUpdatePrice.setOnClickListener {
-            setBackStackData(KEY_PRICE, binding.editTextPricePerCoin.text.toString().toDouble(), true)
+        binding.buttonUpdateFee.setOnClickListener {
+            setBackStackData(KEY_FEE, binding.editTextFee.text.toString().toDouble(), true)
         }
     }
 
     companion object {
-        const val KEY_PRICE = "app.coinfo.feature.transactions.KEY_PRICE"
+        const val KEY_FEE = "app.coinfo.feature.transactions.KEY_FEE"
     }
 }
