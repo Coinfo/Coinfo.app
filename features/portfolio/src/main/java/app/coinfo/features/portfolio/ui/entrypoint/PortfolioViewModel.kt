5843657ab982819ddcc91d7f6d6309042e8d7cf8
Viktor Apoyan <chamich.apps@gmail.com>
2022-03-12 19:54:22 +0100

    Show currency symbol for portfolio assets

--- features/portfolio/src/main/java/app/coinfo/features/portfolio/ui/entrypoint/PortfolioViewModel.kt
+++ features/portfolio/src/main/java/app/coinfo/features/portfolio/ui/entrypoint/PortfolioViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import app.coinfo.library.core.enums.Currency
+import app.coinfo.library.core.ktx.toStringWithSuffix
+import app.coinfo.library.preferences.Preferences
 import app.coinfo.repository.coins.CoinsRepository
 import app.coinfo.repository.portfolios.PortfoliosRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,7 +17,11 @@ import javax.inject.Inject
 class PortfolioViewModel @Inject constructor(
     private val portfoliosRepository: PortfoliosRepository,
     private val coinsRepository: CoinsRepository,
+    private val preferences: Preferences,
 ) : ViewModel() {
+
+    private val currentCurrency: Currency
+        get() = preferences.loadCurrency()
 
     val assets: LiveData<List<UIAssetsItem>>
         get() = _portfolios
@@ -25,18 +31,18 @@ class PortfolioViewModel @Inject constructor(
         viewModelScope.launch {
             val portfolioAssets = portfoliosRepository.loadAssets(portfolioId)
             val coinsMap = coinsRepository.loadCoins(portfolioAssets.coins, Currency.EUR).associateBy { it.id }
-
             _portfolios.value = portfolioAssets.assets.map {
                 val coin = coinsMap[it.coinId]!!
+                val totalPrice = it.getAssetAmount() * coin.currentPrice
                 UIAssetsItem(
                     id = it.coinId,
                     name = coin.name,
                     symbol = coin.symbol,
                     icon = coin.image,
                     totalHoldings = it.getAssetAmount().toString(),
-                    totalPrice = (it.getAssetAmount() * coin.currentPrice).toString(),
+                    totalPrice = "${totalPrice.toStringWithSuffix(2)}${currentCurrency.symbol}",
                     totalProfitLoss = "0",
-                    price = coin.currentPrice.toString()
+                    price = "${coin.currentPrice.toStringWithSuffix(2)}${currentCurrency.symbol}"
                 )
             }
         }
