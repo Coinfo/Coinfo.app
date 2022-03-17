bf1ffb0d43feb8cb32cb564f8b255f0b00b12aa8
Viktor Apoyan <chamich.apps@gmail.com>
2022-03-17 11:29:51 +0100

    Transactions Overview - Total Work for Asset

--- libraries/core/src/main/java/app/coinfo/library/core/ktx/MutableLifeDataKtx.kt
+++ libraries/core/src/main/java/app/coinfo/library/core/ktx/MutableLifeDataKtx.kt
@@ -1,6 +1,10 @@
 package app.coinfo.library.core.ktx
 
 import androidx.lifecycle.MutableLiveData
+import app.coinfo.library.core.enums.Currency
 
 val MutableLiveData<Double>.safeValue
     get() = value ?: 0.0
+
+val MutableLiveData<Currency>.safeValue
+    get() = value ?: Currency.EUR
